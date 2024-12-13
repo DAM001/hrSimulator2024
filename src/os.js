c0d838ac860d5d53a67cee3c9f0995ced376c881
DAM <52104465+DAM001@users.noreply.github.com>
2024-12-13 07:46:21 +0100

    feat: start menu

--- src/os.js
+++ src/os.js
@@ -10,6 +10,8 @@ class OperatingSystem {
     }
 
     openApplication(application) {
+        if (this.applications.includes(application)) return;
+
         this.applications.push(application);
         os.container.appendChild(application.createWindow());
         application.icon = this.taskbar.addApplicationIcon(application);
@@ -27,5 +29,6 @@ const os = new OperatingSystem();
 
 
 // Test usage
-//const testApp = new Application("Test", "../assets/business.png");
+const testApp = new Application("SoftMicro Teams", "../assets/business.png");
+os.taskbar.addMenuIcon(testApp);
 //os.openApplication(testApp);
--- src/taskbar.js
+++ src/taskbar.js
@@ -24,11 +24,28 @@ class Taskbar {
         `;
 
         this.container.appendChild(appHTML);
-
         return appHTML;
     }
 
     toggleMenu() {
         this.menu.classList.toggle("hidden");
+    }
+
+    //MENU
+    addMenuIcon(application) {
+        const iconHTML = document.createElement("div");
+        iconHTML.className = "menu-app-button";
+        iconHTML.innerHTML = `
+            <img src="${application.data.icon}" alt="">
+            <p>${application.data.name}</p>
+        `;
+
+        iconHTML.addEventListener("click", () => {
+            os.openApplication(application);
+            this.toggleMenu();
+        });
+
+        this.menu.querySelector(".menu-apps").appendChild(iconHTML);
+        return iconHTML;
     }
 }
