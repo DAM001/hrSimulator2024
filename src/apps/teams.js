7c2cb99c560569b969ea2fd31b74fe77729eb6b1
DAM <52104465+DAM001@users.noreply.github.com>
2024-12-14 13:41:09 +0100

    feat: WIP teams

--- src/apps/teams.js
+++ src/apps/teams.js
@@ -10,6 +10,34 @@ class Teams extends Application {
             this.sendNotification("Test message", "Description of the message");
         }, 10000);
     }
+
+    createWindow() {
+        const returnValue = super.createWindow();
+        this.setupApplication();
+        return returnValue;
+    }
+
+    setupApplication() {
+        const innerHTML = `
+            <div class="window-content-teams">
+                <div class="sidebar">
+                    <div class="channel">Channel 1</div>
+                    <div class="channel">Channel 2</div>
+                </div>
+                <div class="chat">
+                    <div class="messages">
+                        <div class="message">Hello</div>
+                    </div>
+                    <div class="input-bar">
+                        <input type="text" id="messageInput" placeholder="Type a message...">
+                        <button id="sendBtn">Send</button>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        this.content.innerHTML = innerHTML;
+    }
 }
 
 const softMicroTeams = new Teams("SoftMicro Teams", "./assets/business.png");
